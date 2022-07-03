354435e9d49ab32209f293d0549fa4621b22a334
Gizem Haspolat <gizemhaspolat@gmail.com>
2022-07-04 01:29:44 +0300

    coded along

---
+++ src/components/DisplayData.js
@@ -0,0 +1,9 @@
+export default function DisplayData({ firstName, lastName }) {
+  return (
+    <>
+      <h1>
+        {firstName} {lastName}
+      </h1>
+    </>
+  );
+}
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,14 @@
-import React, { useState } from "react";
-
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
-
+export default function Form({
+  firstName,
+  lastName,
+  handleFirstNameChange,
+  handleLastNameChange,
+}) {
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" onChange={handleFirstNameChange} value={firstName} />
+      <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
   );
 }
-
-export default Form;
